32ca2e0717e591213777df5e07ae4f0777267ab4
d7o1d1s0 <d7sarmy@gmail.com>
2022-02-17 14:10:43 -0500

    clarifying

---
+++ README.md
@@ -0,0 +1,4 @@
+Notes: 
+- Requires ZkScanalyzer (https://github.com/defibuildereth/zkscanalyzer) to be running on localhost:3000
+- Add relevant addresses to 'addresses' array of bulkScraper.js, full history saves to accountsHistory and totalHistory
+- Use crontab or launchd to run automatically every X time
--- bulkScraper.js
+++ bulkScraper.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 var fs = require('fs');
 
 const site = 'http://localhost:3000/';
-const content = [
+const addresses = [
     '0xf270cb0fe88fbbcafaf66a17640de711ee376d3d',
     '0x88d23a44d07f86b2342b4b06bd88b1ea313b6976'
 ];
@@ -12,13 +12,13 @@ let grandTotal = 0;
 
 
 (async () => {
-    for (let i = 0; i < content.length; i++) {
+    for (let i = 0; i < addresses.length; i++) {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(site);
 
 
-        await page.type('#add', content[i]);
+        await page.type('#add', addresses[i]);
         await page.click('#searchsubmit');
         await page.waitForSelector('#myID');
 
@@ -33,7 +33,7 @@ let grandTotal = 0;
         let now = Date.now()
 
         let data2 = extractedText
-        fs.writeFileSync(`./accountsHistory/${content[i]}_${now}.txt`, data2);
+        fs.writeFileSync(`./accountsHistory/${addresses[i]}_${now}.txt`, data2);
         await browser.close();
 
 
